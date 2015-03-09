d207629b49f7aeabf238e0894c2085a611855af4
Dustin Collins <dustinrcollins@gmail.com>
2015-03-09 13:34:36 -0400

    [#89627768] mysql pass needs ${var} syntax

--- db-start.sh
+++ db-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 env=${1:-dev}
-env_u=${env^^}
+env_u=$(echo ${env} | awk '{print toupper($0)}')
 
 echo "Launching ${env} db" > ${env}.log
 
@@ -16,7 +16,7 @@ sleep 10
 docker logs db-${env} >> ${env}.log
 
 password=$(openssl rand -hex 8)
-docker exec db-${env} mysql -e "set password for 'redmine' = password('$password');" 2>&1 >> ${env}.log
+docker exec db-${env} mysql -e "set password for 'redmine' = password('${password}');" 2>&1 >> ${env}.log
 echo export DB_PASSWORD_${env_u}=${password}
 
 mysql_host=$(docker inspect db-${env} | grep IPAddress | cut -d '"' -f 4)
