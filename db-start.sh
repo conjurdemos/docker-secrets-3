25c2d362edb42d30b0bb6f54356127a58c95ed84
Dustin Collins <dustinrcollins@gmail.com>
2015-03-09 13:15:03 -0400

    [#89627768] Use dev as default arg, normalize ${var} syntax

--- db-start.sh
+++ db-start.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -e
 
-env=$1
-env_u=`echo $env | awk '{print toupper($0)}'`
+env=${1:-dev}
+env_u=${env^^}
 
-echo "Launching $env db" > $env.log
+echo "Launching ${env} db" > ${env}.log
 
-if [ -f $env.cid ]; then
- ./db-stop.sh $env
+if [ -f ${env}.cid ]; then
+ ./db-stop.sh ${env}
 fi
 
-docker run -d --cidfile=$env.cid --name=db-$env conjurdemos/redmine-db 2>&1 >> $env.log
+docker run -d --cidfile=${env}.cid --name=db-${env} conjurdemos/redmine-db 2>&1 >> ${env}.log
 
 sleep 10
 
-docker logs db-$env >> $env.log
+docker logs db-${env} >> ${env}.log
 
 password=$(openssl rand -hex 8)
-docker exec db-$env mysql -e "set password for 'redmine' = password('$password');" 2>&1 >> $env.log
-echo export DB_PASSWORD_${env_u}=$password
+docker exec db-${env} mysql -e "set password for 'redmine' = password('$password');" 2>&1 >> ${env}.log
+echo export DB_PASSWORD_${env_u}=${password}
 
-mysql_host=`docker inspect db-$env | grep IPAddress | cut -d '"' -f 4`
+mysql_host=$(docker inspect db-${env} | grep IPAddress | cut -d '"' -f 4)
 
-echo export DB_HOST_${env_u}=$mysql_host
+echo export DB_HOST_${env_u}=${mysql_host}
--- db-stop.sh
+++ db-stop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-env=$1
+env=${1:-dev}
 
-if [ -f $env.cid ]; then
-  cid=$(<$env.cid)
-  echo "Stopping $env db" >> $env.log
-  docker rm -f $cid 2>&1 > /dev/null
-  rm $env.cid
+if [ -f ${env}.cid ]; then
+  cid=$(<${env}.cid)
+  echo "Stopping ${env} db" >> ${env}.log
+  docker rm -f ${cid} 2>&1 > /dev/null
+  rm ${env}.cid
 fi
