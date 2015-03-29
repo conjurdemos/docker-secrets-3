4aa6b635c7050154fbf5e6b16baf5b39cd50f584
Dustin Collins <dustinrcollins@gmail.com>
2015-03-29 11:16:47 -0400

    demo user is already in security_admin group

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-conjur group create security_admin
-conjur group members add security_admin admin
-
 conjur user create --as-group security_admin donna > donna.json
 conjur group create --as-group security_admin developers
 conjur group members add -a developers donna
