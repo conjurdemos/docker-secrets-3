c642fa6c434e20e616f019246792f4396211b20f
Kevin Gilpin <kgilpin@gmail.com>
2015-04-03 15:40:01 +0000

    deploy the docker image as a foundation config plus identity from env vars; update init.sh to be idempotent

--- init.sh
+++ init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-conjur user create --as-group security_admin donna > donna.json
-conjur group create --as-group security_admin developers
+if ! conjur authn whoami &> /dev/null || [ $(conjur authn whoami | jsonfield username) != demo ]; then
+  conjur authn login demo
+fi
+
+./init.rb
+
+if [ $(conjur resource exists user:donna) = false ]; then
+  conjur user create --as-group security_admin donna > donna.json
+fi
+if [ $(conjur resource exists group:developers) = false ]; then
+  conjur group create --as-group security_admin developers
+fi
 conjur group members add -a developers donna
 
 apikey=$(cat donna.json | jsonfield api_key)
